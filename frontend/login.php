3d563bc10ea8bf8dab12076d9a3b46cf28f42aa5
Priyank <pp677@njit.edu>
2022-03-07 16:06:05 -0500

    added login webpage

---
+++ frontend/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="loginStyle.css">
+</head>
+<body>
+    <div class="center">
+        <h1>Login</h1>
+        <form method="post">
+            <div class="textField">
+                <input type="text" required>
+                <label for="username">Username</label>
+            </div>
+            <div class="textField">
+                <input type="text" required>
+                <label for="password">Password</label>
+            </div>
+            <input type="button" value="Login">
+        </form>
+    </div>
+    
+    
+</body>
+</html>
+
+
