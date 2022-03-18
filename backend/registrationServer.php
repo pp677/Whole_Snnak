d622b9b5dbc7ca23733d04f75012dba82d5ec0d3
aaron <at633@njit.edu>
2022-03-18 17:15:25 -0400

    backend updated with server files for user authentication

---
+++ backend/registrationServer.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+require_once('../rmq/path.inc');
+require_once('../rmq/get_host_info.inc');
+require_once('../rmq/rabbitMQLib.inc');
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['username']))
+  {
+    return "ERROR: No username provided";
+  }
+  switch ($request['type'])
+  {
+		case "register":
+			$client = new rabbitMQClient("../rmq/registerToDB.ini", "testServer");
+			$validate = $request;
+			$response = $client->send_request($validate);
+			return $response;
+  }
+}
+
+$server = new rabbitMQServer("../rmq/register.ini","testServer");
+echo "Server started" . PHP_EOL;
+$server->process_requests('requestProcessor');
+exit();
+?>
+
