04bd7602aa9165e99c54cd04474115892ae232c4
aaron <at633@njit.edu>
2022-03-18 16:53:15 -0400

    db directory now has server files for user authentication

---
+++ db/loginDBServer.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+require_once('connectDB.php');
+require_once('../rmq/path.inc');
+require_once('../rmq/get_host_info.inc');
+require_once('../rmq/rabbitMQLib.inc');
+
+function requestProcessor($request)
+{
+	echo "received request".PHP_EOL;
+	$conn = mysqli_connect($host, $user, $pass, $db);
+	$username = $request['username'];
+	$password = $request['password'];
+
+	if (mysqli_connect_errno())
+	{
+		return "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else
+	{
+		echo "Successfully connected to MySQL\nQuerying...";
+		$query = "select * from user where username = '$username'";
+		$loginUser = mysqli_query($query);
+		$result = mysqli_num_rows($loginUser);
+
+		if ($result == 0)
+		{
+			return ('msg'=>'Username or password are invalid. Please retry');
+		}
+		else
+		{
+			$row = mysqli_fetch_array($loginUser);
+			$hash = $row['password'];
+			if (password_verify($password, $hash))
+			{
+				return ('msg'=>'Verified credentials');
+			}
+		}
+}
+
+$server = new rabbitMQServer("../rmq/loginToDB.ini","testServer");
+echo "Server started" . PHP_EOL;
+$server->process_requests('requestProcessor');
+exit();
+?>
+
---
+++ db/registrationDBServer.php
@@ -0,0 +1,41 @@
+#!/usr/bin/php
+<?php
+require_once('connectDB.php');
+require_once('../rmq/path.inc');
+require_once('../rmq/get_host_info.inc');
+require_once('../rmq/rabbitMQLib.inc');
+
+function requestProcessor($request)
+{
+	echo "received request".PHP_EOL;
+	$conn = mysqli_connect($host, $user, $pass, $db);
+	$firstname = $request['firstname'];
+	$lastname = $request['lastname'];
+	$username = $request['username'];
+	$password = $request['password'];
+
+	if (mysqli_connect_errno())
+	{
+		return "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else
+	{
+		echo "Successfully connected to MySQL\nQuerying...";
+		$query = "insert into user values('$firstname', '$lastname', '$username', '$password')";
+		$createUser = mysqli_query($query);
+		if ($createUser)
+		{
+			return 'created';
+		}
+		else
+		{
+			return 'notCreated';
+		}
+}
+
+$server = new rabbitMQServer("../rmq/registerToDB.ini","testServer");
+echo "Server started" . PHP_EOL;
+$server->process_requests('requestProcessor');
+exit();
+?>
+
