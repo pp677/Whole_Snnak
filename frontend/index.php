eae259eba28d4ac1e568490d5707015d1f5abab3
Priyank <pp677@njit.edu>
2022-03-20 17:18:02 -0400

    Fixed up navbar

--- frontend/index.php
+++ frontend/index.php
@@ -4,9 +4,10 @@
 		<!-- Required CSS Link -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <title>Home Page</title>
+<link rel="stylesheet" href="loginStyle.css">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php">Whole Snnak</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
