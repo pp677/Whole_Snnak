f2cf578765bed0579e2c9744ddd7888e753a4506
aaron <at633@njit.edu>
2022-03-21 19:56:12 -0400

    altering seed code to see if a simpler simulation of a DB connection is working

---
+++ RabbitMQSeedCode/php_example/testRabbitMQDB.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return "client received response if printed on console";
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQDB.ini","testServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
+
